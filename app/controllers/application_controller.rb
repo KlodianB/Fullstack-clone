a0639e9fa9a967474f415cf03313f0ee53e7eb2e
Klodian Behrami <klodianbehrami@gmail.com>
2023-09-14 17:52:43 -0400

    adding users controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,27 @@ class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
 
     protect_from_forgery with: :exception
-    before_action :snake_case_params
-    before_action :attach_authenticity_token
+    before_action :snake_case_params, :attach_authenticity_token
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+      end
+    
+      def login!(user)
+        session[:session_token] = user.reset_session_token!
+      end
+    
+      def logout!
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        @current_user = nil
+      end
+    
+      def require_logged_in
+        unless current_user
+          render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+      end
 
     private
     def snake_case_params
