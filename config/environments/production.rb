c03c230b3a90380d31907144010561667478b91f
Klodian Behrami <klodianbehrami@gmail.com>
2023-10-03 17:10:56 -0400

    trying to host on render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  production:
-  <<: *default
-  url: <%= ENV['DATABASE_URL'] %>
+#   production:
+#   <<: *default
+#   url: <%= ENV['DATABASE_URL'] %>
 end
